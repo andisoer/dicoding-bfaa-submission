98b132f27270a9c435d7adc3b37fbfc0e318b7ec
Andi <andi502.as@gmail.com>
2020-08-19 18:15:31 +0700

    add FollowerFragment.kt, add FollowingFragment.kt

--- consumerapp/src/main/java/com/soerjdev/consumerapp/MainActivity.kt
+++ consumerapp/src/main/java/com/soerjdev/consumerapp/MainActivity.kt
@@ -2,11 +2,34 @@ package com.soerjdev.consumerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.soerjdev.consumerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setContentView(binding.root)
+
+        initView()
+        initNavController()
+
     }
+
+    private fun initView() {
+        supportActionBar?.hide()
+    }
+
+    private fun initNavController() {
+        val navController = findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean = findNavController(R.id.navHostFragment).navigateUp()
 }
---
+++ consumerapp/src/main/java/com/soerjdev/consumerapp/UserSearchAdapter.kt
@@ -0,0 +1,51 @@
+package com.soerjdev.consumerapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.soerjdev.dicodingbfaasubmission.R
+import com.soerjdev.dicodingbfaasubmission.data.model.SearchResponse
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_user.view.*
+
+class UserSearchAdapter (private val context: Context, private val listener: Listener) :
+        RecyclerView.Adapter<UserSearchAdapter.ViewHolder> () {
+
+    private var listUser = emptyList<SearchResponse>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_user,
+                parent,
+                false
+            )
+        )
+
+    override fun getItemCount(): Int = listUser.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(listUser[position])
+        holder.itemView.setOnClickListener {
+            listener.onUserClickListenre(it, listUser[position])
+        }
+    }
+
+    class ViewHolder (override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bindItem(item: SearchResponse) {
+            containerView.tvUsernameItemUser.text = item.login
+            containerView.tvUserTypeItemUser.text = item.type
+        }
+    }
+
+    internal fun setUserSearchData(listUser : List<SearchResponse>){
+        this.listUser = listUser
+        notifyDataSetChanged()
+    }
+
+    interface Listener {
+        fun onUserClickListenre(view: View, data: SearchResponse)
+    }
+}
